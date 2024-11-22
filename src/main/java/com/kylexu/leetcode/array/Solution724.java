f468b278b10487b55a8c1d141f0d12a951968fb7
xuguoming <xuguoming@dhgate.com>
2024-11-22 13:48:44 +0800

    1.  提交

---
+++ src/main/java/com/kylexu/leetcode/array/Solution724.java
@@ -0,0 +1,43 @@
+package com.kylexu.leetcode.array;
+
+/**
+ * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+ * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+ * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。*
+ * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1, 7, 3, 6, 5, 6]
+ * 输出：3
+ * 解释：
+ * 中心下标是 3 。
+ * 左侧数之和 sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11 ，
+ * 右侧数之和 sum = nums[4] + nums[5] = 5 + 6 = 11 ，二者相等。
+ */
+public class Solution724 {
+    public static void main(String[] args) {
+//        int[] nums = new int[]{1, 7, 3, 6, 5, 6};
+        int[] nums = new int[]{1, 2, 3};
+        System.out.println(new Solution724().pivotIndex(nums));
+    }
+
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        int leftSum = 0;
+        int rightSum = sum;
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
