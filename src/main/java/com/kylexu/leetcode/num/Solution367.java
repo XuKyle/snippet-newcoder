cd3de1df732fa7f28f181a6a52153b6ae6b6db03
xuguoming <xuguoming@dhgate.com>
2025-01-03 11:28:19 +0800

    1.  继续学习

---
+++ src/main/java/com/kylexu/leetcode/num/Solution367.java
@@ -0,0 +1,44 @@
+package com.kylexu.leetcode.num;
+
+/**
+ * 367. 有效的完全平方数
+ * 简单
+ * 相关标签
+ * 相关企业
+ * 给你一个正整数 num 。如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+ * 完全平方数 是一个可以写成某个整数的平方的整数。换句话说，它可以写成某个整数和自身的乘积。
+ * <p>
+ * 不能使用任何内置的库函数，如  sqrt 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：num = 16
+ * 输出：true
+ * 解释：返回 true ，因为 4 * 4 = 16 且 4 是一个整数。
+ * 示例 2：
+ * <p>
+ * 输入：num = 14
+ * 输出：false
+ * 解释：返回 false ，因为 3.742 * 3.742 = 14 但 3.742 不是一个整数。
+ */
+public class Solution367 {
+    public static void main(String[] args) {
+        System.out.println(new Solution367().isPerfectSquare(14));
+    }
+
+    public boolean isPerfectSquare(int num) {
+        if (num == 1 || num == 0) {
+            return true;
+        }
+
+        for (int i = 0; i <= num / 2; i++) {
+            if (i * i == num) {
+                return true;
+            } else if (i * i > num) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
